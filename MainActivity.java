5b245077e3fca6ba47a5e21401f9298109470686
PreranaGanvatkar <preranaganvatkar@gmail.com>
2021-11-06 16:54:58 +0530

    modified

---
+++ MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.cameraapp1;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+   ImageView image;
+   Button button;
+
+
+    public static final int CAMERA_PERMISSION_CODE=1;
+    public static final int CAMERA_IMAGE_CODE=2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image = findViewById(R.id.imgcamera);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                    if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        String[] permissions = {Manifest.permission.CAMERA};
+                        requestPermissions(permissions, CAMERA_PERMISSION_CODE);
+                    } else {
+                        takePicture();
+                    }
+                }
+
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == CAMERA_PERMISSION_CODE){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                takePicture();
+            }else{
+                Toast.makeText(MainActivity.this, "App need Permission", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==CAMERA_IMAGE_CODE){
+            if(resultCode==RESULT_OK){
+                Bitmap takenImage =(Bitmap) data.getExtras().get("data");
+                image.setImageBitmap(takenImage);
+            }
+        }
+
+    }
+
+    private void takePicture(){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent,CAMERA_IMAGE_CODE);
+
+    }
+}
